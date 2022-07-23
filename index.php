b5c12108f57831ddf902e933f0f71bc906ad4c96
kanji-hirayama <kanji.hirayama@rechroma.co.jp>
2022-07-23 09:39:31 +0900

    test0723

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>リクロマ株式会社</title>
+    <meta name="description" content="~">
+    <link rel="stylesheet" href="./assets/css/sanitize.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="icon" type="image/png" href="./assets/images/favicon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header class="page-header" >
+        <a href="index.html" class="logo"><img src="./assets/images/logo.png" alt="リクロマ株式会社"></a>
+        <ul class="main-nav">
+            <li><a href="">会社紹介</a></li>
+            <li><a href="">サービス</a></li>
+            <li><a href="">ナレッジ</a></li>
+            <li><a href="">採用情報</a></li>
+            <li><a href="">サイト内検索</a></li>
+            <li><a href="">お問い合せ</a></li>
+        </ul>
+    </header>
+    <main>
+        <div>
+            <h1>気候変動時代に求められる共創価値を提供することで社会に貢献する</h1>
+            <p>TCFD開示支援、スコープ1,2,3算定・削減支援、気候変動に関する社内研修事業を行っています</p>
+            <img src="./assets/images/background.jpg" alt="">
+        </div>
+        <div>
+            <h2>気候変動対応でこのようなお悩みはございませんか</h2>
+            <p>気候変動対応に関する3つの課題</p>
+            <div>
+                <h3>社内での共通認識が取れていない</h3>
+                <p>気候変動</p>
+            </div>
+            <div>
+                <h3>社内での共通認識が取れていない</h3>
+                <p>気候変動</p>
+            </div>
+            <div>
+                <h3>社内での共通認識が取れていない</h3>
+                <p>気候変動</p>
+            </div>
+        </div>
+        <section class="section">
+            <h1 class="section-headline">ミッション・ビジョン</h1>
+            <figure class="mission-vision">
+                <figcaption class="mission-vision-caption">
+                    <h2>気候変動時代に求められる情報</h2>
+                    <p class="misson-vision-description">私たちは「気候変動時代に求められる情報を提供することで社会に貢献する」を企業理念に掲げています。カーボンニュートラルの達成のためには企業、投資家、一般消費者など多くのステークホルダーの協働が必要であり、当社は情報を提供することで協働を促進していきます。</p>
+                </figcaption>
+                <img class="mission-vision-image" src="./assets/images/qws.png" alt="ミッション・ビジョン">
+            </figure>
+        </section>
+        <section class="section">
+            <h1 class="section-headline">会社紹介</h1>
+            <figure class="company">
+                <img class="company-image" src="./assets/images/kato.jpeg" alt="会社紹介">
+                <figcaption class="company-caption">
+                    <h2 class="company-headline">代表取締役　加藤 貴大</h2>
+                    <p class="company-description">大学卒業後、PwC Mexico International Business Centreにて日系企業への法人営業 / アドバイザリー業務に携わる。帰国後、一般社団法人CDP Worldwide-Japanを経て、リクロマ株式会社（旧：株式会社ウィズアクア）を創業。大学在学中にはNPO法人AIESEC in Japanの事務局次長として1,700人を擁する団体の組織開発に従事。1992年生まれ。開成中学校・高等学校、慶應義塾大学経済学部卒業</p>
+                </figcaption>
+            </figure>
+            <table boder="1">
+                <tr>
+                    <td>会社名</td>
+                    <td>リクロマ株式会社 / ReChroma Corporation.</td>
+                </tr>
+                <tr>
+                    <td>設立日</td>
+                    <td>2018年5月</td>
+                </tr>
+                <tr>
+                    <td>資本金</td>
+                    <td>1,000万円</td>
+                </tr>
+                <tr>
+                    <td>主要株主</td>
+                    <td>加藤 貴大<br>Reapra Japan</td>
+                </tr>
+                <tr>
+                    <td>代表者</td>
+                    <td>加藤 貴大</td>
+                </tr>
+                <tr>
+                    <td>所在地</td>
+                    <td>登記所：<br>〒150-0001 東京都渋谷区神宮前六丁目23番4号 桑野ビル2階<br>事業所：<br>〒150-0002 東京都渋谷区渋谷二丁目24番12号 渋谷スクランブルスクエア 15階</td>
+                </tr>
+            </table>
+        </section>
+        <div>
+            <h2>アドバイザー</h2>
+            <p></p>
+        </div>
+        <div>
+            <h2>メンバー</h2>
+            <p></p>
+        </div>
+        <div class="help">
+            企業名：<input type="text">
+            姓：<input type="text">
+            名：<input type="text">
+            メールアドレス：<input type="email">
+            電話番号：<input type="tel">
+            部署：<input type="text">
+            役職：<input type="radio"> 部長
+            きっかけ：<input type="text">
+            お問合せ内容：<input type="text">
+            面談希望：<input type="text">
+            <input type="submit" value="送信する">
+        </div>
+    </main>
+    <footer>
+        <p>リクロマ株式会社</p>
+    </footer>
+    <script scr="./assets/js/app.js"></script>
+</body>
+</html>
